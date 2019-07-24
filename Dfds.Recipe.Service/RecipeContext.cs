85b57939c056b360db85cdde2c982edbbe859c48
Fozzy <stfos@DFDS.COM>
2019-07-24 15:18:52 +0100

    Initial commit.

---
+++ Dfds.Recipe.Service/RecipeContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dfds.Recipe.Service
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ Dfds.Recipe.Tests/Utils.cs
@@ -0,0 +1,19 @@
+﻿using Dfds.Recipe.Service;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Dfds.Recipe.Tests
+{
+    public static class Utils
+    {
+        public static RecipeContext GetNewInMemoryContextInstance()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<RecipeContext>();
+            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new RecipeContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
